4053e0902c0e0a3091f35836b3195e88daa3aa77
Wizzel1 <r_christian@gmx.de>
2026-05-19 20:26:45 +0200

    Add font config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,17 @@
 // @ts-check
-import { defineConfig, fontProviders } from 'astro/config';
+import { defineConfig, fontProviders } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  image: {
-    remotePatterns: [{ hostname: 'www.depero.de' }],
-  },
   fonts: [
     {
       provider: fontProviders.google(),
-      name: 'Averia Serif Libre',
-      cssVariable: '--font-heading',
+      name: "Averia Serif Libre",
+      cssVariable: "--font-heading",
       weights: [300, 400, 700],
-      styles: ['normal', 'italic'],
-      subsets: ['latin'],
-      fallbacks: ['serif'],
+      styles: ["normal", "italic"],
+      subsets: ["latin"],
+      fallbacks: ["serif"],
     },
   ],
 });
