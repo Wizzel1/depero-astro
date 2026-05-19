acdc4484af8cef42d8f6eb5aac44573dbbc96a8c
Wizzel1 <r_christian@gmx.de>
2026-05-19 15:23:04 +0200

    Use astro fontProvider to reduce render blocking requests

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,20 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  image: {
+    remotePatterns: [{ hostname: 'www.depero.de' }],
+  },
+  fonts: [
+    {
+      provider: fontProviders.google(),
+      name: 'Averia Serif Libre',
+      cssVariable: '--font-heading',
+      weights: [300, 400, 700],
+      styles: ['normal', 'italic'],
+      subsets: ['latin'],
+      fallbacks: ['serif'],
+    },
+  ],
+});
